2766cfa6bf93d2bc1055d9a660a7c728d2a18072
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-08-01 12:12:03 -0500

    create links to question pages

--- client/cooking-app/src/App.js
+++ client/cooking-app/src/App.js
@@ -1,72 +1,22 @@
 // Don't forget to import useState and useEffect
 import React, { useState, useEffect } from "react";
 import $ from "jquery";
+import Home from "./Home";
+import Question from "./Question";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  // Create state for both API fields one by one using useState
-  // const [status, setStatus] = useState(null);
-  // const [version, setVersion] = useState(null);
-
-  const [question, setQuestion] = useState("");
-  const [questions, setQuestions] = useState([]);
-
-  const handleQuestion = (event) => {
-    setQuestion(event.target.value);
-  };
-
-  const saveQuestion = async () => {
-    const response = await $.post("http://localhost:8000/savequestion", {
-      question_text: question,
-      question_created: new Date(),
-    });
-    console.log(response);
-  };
-
-  // useEffect replaces componentDidMount and componentDidUpdate
-  useEffect(() => {
-    // fetch("http://localhost:8000")
-    //   .then((data) => data.json())
-    //   .then((response) => {
-    //     console.log(response);
-    // setStatus(response.status);
-    //   // setVersion(response.version);
-    // });
-  });
-
-  // Render the two API fields
-  // const statusText = status
-  //   ? "The server is working"
-  //   : "The server is not working";
-
-  const fetchQuestions = () => {
-    const resp = fetch("http://localhost:8000/getallquestions")
-      .then((data) => data.json())
-      .then((response) => {
-        console.log(response);
-        setQuestions(response);
-      });
-  };
-  return (
-    <div className="App">
-      <form>
-        <label>
-          Question:
-          <input type="text" onChange={handleQuestion} />
-        </label>
-      </form>
-      <br></br>
-      <button onClick={saveQuestion}>Save</button>
-      <br></br>
-      <button onClick={fetchQuestions}>Get Question</button>
-      <br></br>
-      <div>
-        {questions.map((q) => (
-          <p key={q.question_id}>{q.question_text}</p>
-        ))}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/question" exact component={Question} />
+        </Router>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ client/cooking-app/src/Home.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import $ from "jquery";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [question, setQuestion] = useState("");
+  const [questions, setQuestions] = useState([]);
+
+  const handleQuestion = (event) => {
+    setQuestion(event.target.value);
+  };
+
+  const saveQuestion = async () => {
+    const response = await $.post("http://localhost:8000/savequestion", {
+      question_text: question,
+      question_created: new Date(),
+    });
+    console.log(response);
+  };
+
+  useEffect(() => {});
+
+  const fetchQuestions = () => {
+    const resp = fetch("http://localhost:8000/getallquestions")
+      .then((data) => data.json())
+      .then((response) => {
+        console.log(response);
+        setQuestions(response);
+      });
+  };
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Question:
+          <input type="text" onChange={handleQuestion} />
+        </label>
+      </form>
+      <br></br>
+      <button onClick={saveQuestion}>Save</button>
+      <br></br>
+      <button onClick={fetchQuestions}>Get Question</button>
+      <br></br>
+      <div>
+        {questions.map((q) => (
+          <p key={q.question_id}>
+            <Link to="/question/">{q.question_text}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
