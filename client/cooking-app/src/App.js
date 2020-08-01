82932baa86904b0b34cd0f439296acf9844d5633
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-08-01 10:59:55 -0500

    write questions to database

--- client/cooking-app/src/App.js
+++ client/cooking-app/src/App.js
@@ -1,31 +1,52 @@
 // Don't forget to import useState and useEffect
 import React, { useState, useEffect } from "react";
+import $ from "jquery";
 import "./App.css";
 
 function App() {
   // Create state for both API fields one by one using useState
-  const [status, setStatus] = useState(null);
-  const [version, setVersion] = useState(null);
+  // const [status, setStatus] = useState(null);
+  // const [version, setVersion] = useState(null);
+
+  const [question, setQuestion] = useState("");
+
+  const handleQuestion = (event) => {
+    setQuestion(event.target.value);
+  };
+
+  const saveQuestion = async () => {
+    const response = await $.post("http://localhost:8000/savequestion", {
+      question_text: question,
+      question_created: new Date(),
+    });
+    console.log(response);
+  };
 
   // useEffect replaces componentDidMount and componentDidUpdate
   useEffect(() => {
-    fetch("http://localhost:8000")
-      .then((data) => data.json())
-      .then((response) => {
-        console.log(response);
-        setStatus(response.status);
-        setVersion(response.version);
-      });
+    // fetch("http://localhost:8000")
+    //   .then((data) => data.json())
+    //   .then((response) => {
+    //     console.log(response);
+    // setStatus(response.status);
+    //   // setVersion(response.version);
+    // });
   });
 
   // Render the two API fields
-  const statusText = status
-    ? "The server is working"
-    : "The server is not working";
+  // const statusText = status
+  //   ? "The server is working"
+  //   : "The server is not working";
   return (
     <div className="App">
-      <p>Status: {statusText}</p>
-      <p>Version: {version}</p>
+      <form>
+        <label>
+          Question:
+          <input type="text" onChange={handleQuestion} />
+        </label>
+      </form>
+      <br></br>
+      <button onClick={saveQuestion}>Save</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,39 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 // Create an express app
 var app = express();
 var port = 8000;
 
+var sqlite3 = require("sqlite3");
+var db = new sqlite3.Database("cooking.db");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // Launch the express app on a port
 app.listen(port, () => console.log(`Listening on ${port}`));
+
+app.post("/savequestion", function (request, response) {
+  console.log("New question: " + request.body.question_text);
+  db.run(
+    "INSERT INTO questions (question_text, question_created) VALUES (?,?)",
+    [request.body.question_text, request.body.question_created],
+    function (err) {
+      if (err) {
+        console.log(err.message);
+      } else {
+        response.json({
+          question: request.body.question_text,
+          id: this.lastID,
+        });
+      }
+    }
+  );
+});
 
 // Same request handler as in case of an HTTP server
 var requestHandler = function (request, response) {
@@ -21,18 +49,18 @@ var requestHandler = function (request, response) {
 // These are the routes (first arg: path, second: callback function)
 app.get("/", requestHandler);
 
-app.get("/pets", function (request, response) {
-  console.log(request.url);
-  const responseObject = [
-    {
-      name: "Bini",
-      type: "Bunny",
-    },
-    {
-      name: "Siku",
-      type: "Polar bear",
-    },
-  ];
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.end(JSON.stringify(responseObject));
-});
+// app.get("/pets", function (request, response) {
+//   console.log(request.url);
+//   const responseObject = [
+//     {
+//       name: "Bini",
+//       type: "Bunny",
+//     },
+//     {
+//       name: "Siku",
+//       type: "Polar bear",
+//     },
+//   ];
+//   response.setHeader("Access-Control-Allow-Origin", "*");
+//   response.end(JSON.stringify(responseObject));
+// });
